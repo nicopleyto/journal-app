da74e6c8bbb5e213be279fcd74c2256836819fee
nicopleyto <nicopleyto@gmail.com>
2022-03-07 18:30:37 +0800

    changed categories index layout & categories validation, added icons

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
     belongs_to :user
     has_many :entries, dependent: :destroy
-    validates :title, presence: true, length: {maximum: 50}
-    validates :description, length: {maximum: 280}
+    validates :title, presence: true, length: {maximum: 17}
+    validates :description, length: {maximum: 150}
 end
