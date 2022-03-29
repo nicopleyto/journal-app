1f30daaa1fdf47e838418ddacf60ba83eedd31c6
nicopleyto <nicopleyto@gmail.com>
2022-03-29 16:36:27 +0800

    removed description from categories model. generated migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_25_064326) do
+ActiveRecord::Schema.define(version: 2022_03_29_083457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
